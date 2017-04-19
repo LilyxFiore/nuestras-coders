5b169d289409b86cccfa0c4b93f9be987f21994c
LilyxFiore <liliana.fiorela@gmail.com>
2017-04-18 23:00:20 -0500

    modificando js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,26 @@
 var content = document.getElementById("content");
-var fotos = ["foto1.jpg", "foto2.jpg", "foto3.jpg", "foto4.jpg", "foto5.jpg", "foto6.jpg"];
 
-for(i = 0; i < fotos.length; i++){
-  addImages("assets/img/"+fotos[i]);
-}
+/** Agregando titulo "Nuestras coders" **/
+var titulo = document.createElement("h1");
+titulo.appendChild(document.createTextNode("Nuestras coders"));
+content.appendChild(titulo);
 
-function addImages(src){
+/** Agregando fotos **/
+var fotos = [{src: "foto1.jpg", name:"Anais Araya"}, {src: "foto2.jpg", name:"Caterine da Silva"},{src: "foto3.jpg", name:"Andre Miranda"},
+            {src: "foto4.jpg", name:"Francisca Ojeda"},{src: "foto5.jpg", name:"Daniela Sánchez"},{src: "foto6.jpg", name:"Berenisse Ríos"}];
+
+fotos.forEach(function(e){
+  addImages("assets/img/"+e.src, e.name);
+});
+
+function addImages(src, name){
+  var figure = document.createElement("figure");
   var img = document.createElement("img");
+  var figcaption = document.createElement("figcaption");
   img.src=src;
-  img.setAttribute("class","foto");
-  content.appendChild(img);
+  figure.setAttribute("class","foto");
+  figure.appendChild(img);
+  figcaption.appendChild(document.createTextNode(name));
+  figure.appendChild(figcaption);
+  content.appendChild(figure);
 }
